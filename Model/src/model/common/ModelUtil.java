b7650b0b2a5c572414fdd07c96d5988bcfbdbb02
Kuuzmma <lion3897@gmail.com>
2021-07-13 13:23:24 +0200

    entity inicijalni

--- Model/src/model/common/ModelUtil.java
+++ Model/src/model/common/ModelUtil.java
@@ -2,15 +2,23 @@ package model.common;
 
 import java.math.BigDecimal;
 
+
+import java.sql.Timestamp;
+
 import oracle.jbo.server.DBTransaction;
 import oracle.jbo.server.SequenceImpl;
 
-
 public class ModelUtil 
 {
-    public static BigDecimal getSequanceNextVal(DBTransaction transaction)
+    public static BigDecimal getSequaenceNextVal(DBTransaction transaction)
     {
-        SequenceImpl seq = new SequenceImpl("ID_SEQ", transaction);
-        return new BigDecimal(seq.getSequenceNumber().toString());
+         SequenceImpl seq = new SequenceImpl("ID_SEQ", transaction);
+         return new BigDecimal(seq.getSequenceNumber().toString());
+    }
+    
+    public static Timestamp getCurrentDate()
+    {
+        java.util.Date today = new java.util.Date();
+        return new Timestamp(today.getTime());
     }
 }
