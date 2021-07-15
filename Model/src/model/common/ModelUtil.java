5621d353db9787083b3ca9ec6eb97ca8f5c7a3ff
Kuuzmma <lion3897@gmail.com>
2021-07-15 15:29:31 +0200

    search finish

--- Model/src/model/common/ModelUtil.java
+++ Model/src/model/common/ModelUtil.java
@@ -3,6 +3,8 @@ package model.common;
 import java.math.BigDecimal;
 
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Timestamp;
 
 import oracle.jbo.server.DBTransaction;
@@ -21,4 +23,35 @@ public class ModelUtil
         java.util.Date today = new java.util.Date();
         return new Timestamp(today.getTime());
     }
+    
+    public static String getSqlDescription(String sql, DBTransaction trans)
+      {
+        PreparedStatement stat = null;
+        ResultSet rs = null;
+        try
+        {
+          stat = trans.createPreparedStatement(sql, 1);
+          rs = stat.executeQuery();
+          while (rs.next())
+          {
+            return rs.getString(1);
+          }
+        }
+        catch (Exception e)
+        {
+          e.printStackTrace();
+        }
+        finally
+        {
+          try
+          {
+            rs.close();
+            stat.close();
+          }
+          catch (Exception e)
+          {
+          }
+        }
+        return "";
+      }
 }
